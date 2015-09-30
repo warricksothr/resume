85fa0055a781a8b09e309244792ffe899b13fab0
Drew Short <warrick@sothr.com>
2015-09-30 18:43:29 -0500

    Tweaking script

--- package-and-deploy.sh
+++ package-and-deploy.sh
@@ -9,8 +9,22 @@
 # commit the changes
 # push the changes to github
 
-cp DPSResume.pdf ..
-cp ResumeSite.tar.gz ..
+cp DPSResume.pdf ~
+cp ResumeSite.tar.gz ~
 
-git clean -f .
+# Prepare the current directory
+git clean -fq .
+git clean -Xq
 git checkout .
+
+#Checkout the deployment directory
+git checkout tags/gh-pages
+
+# Copy the files
+tar -zxvf ~/ResumeSite.tar.gz .
+cp ~/DPSResume.pdf .
+
+# Add the files
+git add .
+git commit -m "Auto updating resume site files"
+git push origin gh-pages
