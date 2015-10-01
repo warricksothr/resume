50eb8e6d868d450f335e1843e5e28c218dcc5c35
Drew Short <warrick@sothr.com>
2015-09-30 20:39:23 -0500

    Changing the zoom to 1.5x

--- build-html-docker.sh
+++ build-html-docker.sh
@@ -5,7 +5,7 @@ cd /source
 userid="$(cat userid.info)"
 
 # Script to build an HTML site from the PDF source
-pdf2htmlEX --embed cfijo --dest-dir out DPSResume.pdf
+pdf2htmlEX --zoom 1.5 --embed cfijo --dest-dir out DPSResume.pdf
 chown -R $userid:$userid out/
 chmod -R 666 out/
 chmod u+x out/
