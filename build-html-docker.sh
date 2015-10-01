9f3658386c21e3f7957478b909b90913ed978f16
Drew Short <warrick@sothr.com>
2015-09-30 20:55:05 -0500

    renaming files

--- build-html-docker.sh
+++ build-html-docker.sh
@@ -9,6 +9,7 @@ pdf2htmlEX --zoom 1.5 --embed cfijo --dest-dir out DPSResume.pdf
 chown -R $userid:$userid out/
 chmod -R 666 out/
 chmod u+x out/
+mv out/DPSResume.html out/index.html
 
 tar -czvf ResumeSite.tar.gz -C out/ .
 chown $userid:$userid ResumeSite.tar.gz
