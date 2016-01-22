78ec391dc8dc7d858421aed6f14dc2c7020ef2ef
Drew Short <warrick@sothr.com>
2016-01-21 20:19:29 -0600

    Working on improving the process of joining the PDFs

--- build-pdf-resume.sh
+++ build-pdf-resume.sh
@@ -3,6 +3,6 @@
 echo "$(id -u)" > userid.info
 
 # Script to build a PDF from the latex source
-docker run -v $(pwd):/source schickling/latex /usr/bin/env bash /source/build-pdf-docker.sh
+docker run -v $(pwd):/source schickling/latex /usr/bin/env bash /source/build-pdf-resume-docker.sh
 
 rm userid.info
--- build-resume-with-cover-letter-pdf-docker.sh
+++ build-resume-with-cover-letter-pdf-docker.sh
@@ -5,16 +5,17 @@ cd /source
 userid="$(cat userid.info)"
 
 # Get PDFJam to combine the pdf's
-curl -LSO http://www2.warwick.ac.uk/fac/sci/statistics/staff/academic/firth/software/pdfjam/pdfjam_latest.tgz
-tar -zxvf pdfjam_latest.tgz
-export PATH="$PWD/pdfjam/bin:$PATH"
+#curl -LSO http://www2.warwick.ac.uk/fac/sci/statistics/staff/academic/firth/software/pdfjam/pdfjam_latest.tgz
+#tar -zxvf pdfjam_latest.tgz
+#export PATH="$PWD/pdfjam/bin:$PATH"
 
 # Script to build a PDF from the latex source
 latex --output-format pdf DPSResume.tex
 latex --output-format pdf ${1}.tex
 
-outfile="DPSResume_With_Coverletter.pdf"
-pdfjoin ${1}.pdf DPSResume.pdf --outfile $outfile
+outfile="${1}_with_Resume.pdf"
+pdfjam --fitpaper 'true' --rotateoversize 'false' --suffix joined --no-landscape --outfile $outfile ${1}.pdf DPSResume.pdf
+#pdfjoin ${1}.pdf DPSResume.pdf --no-landscape --outfile $outfile
 
 chown $userid:$userid $outfile
 chmod 666 $outfile
